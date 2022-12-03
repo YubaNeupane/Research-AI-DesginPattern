2c2fa77cddac79204b6489fcaedce9913a589b23
Yuba Neupane <neopaneyb2@gmail.com>
2022-12-03 11:47:49 -0500

    added env

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+import os
 from GitHubRequestor import *
-githubToken = "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"
-requestor = GitHubRequestor(githubToken)
+load_dotenv()
+
+
+requestor = GitHubRequestor(os.getenv("GitHubToken"))
 
 repo = "PyGithub/PyGithub"
 
